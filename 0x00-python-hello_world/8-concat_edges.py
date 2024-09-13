f281501e609c729dae6b1e8b4533eb90d56e585c
Omnilutiondev <ike.john.agu@gmail.com>
2024-09-13 07:46:57 +0100

    Python hello world create a sentence from a word task 7

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[39:67] + str[107:112] + str[0:7])
+str1 = (str[39:67])
+str2 = (str[107:112])
+str3 = (str[0:7])
+str = (str1 + str2 + str3)
 print(str)
