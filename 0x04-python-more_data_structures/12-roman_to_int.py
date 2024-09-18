3b15907625157a7e47ebd742750e33b245f6f2c2
Omnilutiondev <ike.john.agu@gmail.com>
2024-09-18 22:54:03 +0100

    Python more data structures - task 13 completed - Roman integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,6 @@ def roman_to_int(roman_string):
     numstr = 0
     romdigits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for px in reversed(roman_string):
-    numstr = romdigits[px]
-    totalstr += numstr 
-        if totalstr < numstr * 5 
-            else -numstr
+        numstr = romdigits[px]
+        totalstr += numstr if totalstr < numstr * 5 else -numstr
     return total
