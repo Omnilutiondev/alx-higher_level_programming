f807b3d0c3ed0ad6dd6cc0eccb32cd4eb490dd9c
Omnilutiondev <ike.john.agu@gmail.com>
2024-09-18 05:01:15 +0100

    Python data structures - Remove characters C and c from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    rem = ""
+    for px in range(len(my_string)):
+        if (my_string[px] != 'c' and my_string[px] != 'C'):
+            rem += my_string[px]
+
+        return rem
