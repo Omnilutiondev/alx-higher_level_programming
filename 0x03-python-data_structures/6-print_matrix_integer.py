caa1953bba0089eca7a52e802806036ee8643031
Omnilutiondev <ike.john.agu@gmail.com>
2024-09-18 05:32:14 +0100

    Python data structures - Print matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+        for px in range(len(submatrix)):
+            print("{:d}".format(submatrix[px]),
+                    end="\n" if px is len(submatrix) - 1 else " ")
