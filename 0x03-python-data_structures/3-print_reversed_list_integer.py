6cd940d9b486d10cc3e2ba351bced38cbd1fe11b
Omnilutiondev <ike.john.agu@gmail.com>
2024-09-18 04:44:00 +0100

    Python data structures - Print list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
     for px in reversed(my_list):
-        print(":d".format(px))
+        print("{:d}".format(px))
