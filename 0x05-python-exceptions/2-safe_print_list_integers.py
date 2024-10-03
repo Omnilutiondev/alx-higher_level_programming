c7b15b49760a58ab1ff6955de57dd7c0e66e2135
Omnilutiondev <ike.john.agu@gmail.com>
2024-10-03 02:18:07 +0100

    Python Exceptions task 3 safe print

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx, cdx = 0, 0
+    while idx < x:
+        try:
+            print("{:d}".format(my_list[idx]), end='')
+            cdx += 1
+        except (ValueError, TypeError):
+            pass
+        idx += 1
+    print()
+    return cdx
