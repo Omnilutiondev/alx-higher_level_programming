f010527a863494d51b28382d8b3734e833ab9c95
omnilutiondev <omnilutiondev@ULUNMA>
2024-10-16 21:46:00 +0100

    Task 7 - Completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arglist = list(sys.argv[1:])
 
 try:
-    old_info = load_from_json_file('add_item.json')
+    old_data = load_from_json_file('add_item.json')
 except Exception:
-    old_info = []
+    old_data = []
 
-old_info.extend(arglist)
-save_to_json_file(old_info, 'add_item.json')
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
