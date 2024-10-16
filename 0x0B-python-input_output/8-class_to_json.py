8cec0f38324546df6ae115b0f48055613b282e26
omnilutiondev <omnilutiondev@ULUNMA>
2024-10-16 22:13:27 +0100

    Python Input_Output - Task 8 Completed

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 with simple data structure (list, dictionary, string,
 integer and boolean) for JSON serialization of an object"""
 
+
 def class_to_json(obj):
     """contains the class_to_json"""
     return obj.__dict__
