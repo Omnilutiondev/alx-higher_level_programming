d4b8d519f30c9fe1df389e779dc8f0e8dd2e01a8
omnilutiondev <omnilutiondev@ULUNMA>
2024-10-16 21:18:46 +0100

    Task 6 Completed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """write an object to a text file"""
-    with open(filename, 'z', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
