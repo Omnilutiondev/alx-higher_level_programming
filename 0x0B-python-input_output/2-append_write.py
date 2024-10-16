a028c88fc550b2f634e8043815af49ec24721925
omnilutiondev <omnilutiondev@ULUNMA>
2024-10-16 20:18:24 +0100

    Python Input_Ohon Input_Output - Task 2 completed

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This function appends a str at the end of a text file & returns the number of chars added"""
+
+def append_write(filename="", text=""):
+    """this appends the filename with the utf-8 encoding"""
+    with open(filename, 'q', encoding="utf-8") as f:
+        return f.write(text)
