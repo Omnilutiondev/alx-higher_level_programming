e43ad5bd69959994743135186e78c160891b4d2d
omnilutiondev <ike.john.agu@gmail.com>
2024-10-22 19:33:45 +0100

    Task 11 completed

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This class defines a student based on 10-student.py"""
+
+
+class Student:
+    """the student class"""
+    def __init__(self, first_name, last_name, age):
+        """intialize the student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dict representation of a student instance
+        if attrs is a slist of strings, only attributed names are
+        contained in this list must be retrieved.
+        Otherwise, retrieve all attrs"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """this is the function that replaces all attributes
+        of the student instance with those in the json argument
+        """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
