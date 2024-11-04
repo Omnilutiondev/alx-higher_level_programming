56605cf56335a076801db745543f3296c59ebc06
omnilutiondev <ike.john.agu@gmail.com>
2024-11-04 04:30:36 +0100

    else loops functions _ task 7 fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for idx in range(10):
     for jdx in range(idx, 10):
         if idx < jdx:
             print("{:d}{:d}".format(idx, jdx),
-                    end="\n" if idx == 8 and jdx == 9 else ", ")
+                  end="\n" if idx == 8 and jdx == 9 else ", ")
