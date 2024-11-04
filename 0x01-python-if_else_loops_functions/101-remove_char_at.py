0d6578e4bdb21d6271de94f01ee38c57b865a92e
omnilutiondev <ike.john.agu@gmail.com>
2024-11-04 13:17:18 +0100

    else loops functions _ task 16

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     newstrng = ""
-    for idx, cdx in enumerate(str):
+    for idx, c in enumerate(str):
         if idx != n:
-            newstrng += cdx
+            newstrng += c
             return newstrng
