b3d1b17a201062d2a8acf223c1b6b2f3764af37c
omnilutiondev <ike.john.agu@gmail.com>
2024-11-04 11:33:59 +0100

    else loops functions _ task 13

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for idx in range(1, 101):
+    for idx in range(1, 99):
         if idx % 15 == 0:
             print("FizzBuzz", end=" ")
         elif idx % 5 == 0:
