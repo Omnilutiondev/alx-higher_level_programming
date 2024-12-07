12c6297c0c3a00d2b0188078b0a98fde1dcf764b
omnilutiondev <ike.john.agu@gmail.com>
2024-12-07 05:28:59 +0100

    Python Inheritance task 5 completed

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module is for inherits_from method.'''
+
+
+def inherits_from(obj, a_class):
+    '''Determine if an object is a true subclass of a class.'''
+    return isinstance(obj, a_class) and type(obj) != a_class
