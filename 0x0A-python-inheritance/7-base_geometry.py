d1e1175f89ea4128ffc206a1a53b05581578b1df
omnilutiondev <ike.john.agu@gmail.com>
2024-12-07 05:39:41 +0100

    Python Inheritance task 8 completed

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''The func for BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''A BaseGeometry class.'''
+    def area(self):
+        '''Parameters to compute this area.'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Parameters for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
