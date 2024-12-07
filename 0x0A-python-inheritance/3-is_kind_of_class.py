642431612362cbfb39f0818eebef6a423f5b69a6
omnilutiondev <ike.john.agu@gmail.com>
2024-12-07 05:27:28 +0100

    Python Inheritance task 4 completed

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module is for is_kind_of_class method.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Determine if an object is a subclass of a class.'''
+    return isinstance(obj, a_class)
