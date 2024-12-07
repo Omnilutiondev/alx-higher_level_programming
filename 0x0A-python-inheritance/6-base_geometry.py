89ef144bf3714e034b0526fb5e5340a1c05e4b43
omnilutiondev <ike.john.agu@gmail.com>
2024-12-07 05:33:24 +0100

    Python Inheritance task 7 completed

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''The func for BaseGeometry class.'''
+
+
+class BaseGeometry:
+    '''A BaseGeometry class.'''
+    def area(self):
+        '''Parameters to compute this area.'''
+        raise Exception('area() is not implemented')
