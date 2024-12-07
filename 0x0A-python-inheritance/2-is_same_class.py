f379056b82160d1212bdf14d03430e3fee543208
omnilutiondev <ike.john.agu@gmail.com>
2024-12-07 05:17:25 +0100

    Python Inheritance task 3 completed

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Function for is_same_class method.'''
+
+
+def is_same_class(obj, a_class):
+    '''Checks if an object is exactly like instance of a class.'''
+    return type(obj) == a_class
