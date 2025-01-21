92dc83060feff8955b64bb0e873a4952b1e6197c
Omnilutiondev <ike.john.agu@gmail.com>
2025-01-21 04:16:02 +0100

    Javascript Objects Scopes and Closures - Task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const ttlist = Object.entries(dict);
-const vaals = Object.values(dict);
-const valUniq = [...new Set(vaals)];
-const newDict = {};
-for (const q in valUniq) {
-  const list = [];
-  for (const w in ttlist) {
-    if (ttlist[w][1] === valUniq[q]) {
-      list.unshift(ttlist[w][0]);
-    }
-  }
-  newDict[valUniq[q]] = list;
-}
+const newDict = Object.values(dict).reduce((acc, value) => {
+  acc[value] = Object.entries(dict)
+    .filter(([, v]) => v === value)
+    .map(([z]) => z);
+  return acc;
+}, {});
+
 console.log(newDict);
