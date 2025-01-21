3282e970137eb7178757c511f3834159b0ef439c
Omnilutiondev <ike.john.agu@gmail.com>
2025-01-21 18:43:24 +0100

    0x10-python-network_0 - Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This function finds a peak within a list of unsorted integers """
+
+
+def find_peak(list_of_integers):
+    """ find the number with the shortest algorithm"""
+    if list_of_integers == []:
+        return None
+
+    idx = len(list_of_integers)
+    if idx == 0:
+        return (None)
+    elif idx == 1:
+        return (list_of_integers[0])
+    elif idx == 2:
+        return max(list_of_integers)
+
+    med = int(idx/2)
+    pk = list_of_integers[mid]
+    mylist = list_of_integers
+    if pk > mylist[med - 1] and pk > mylist[med + 1]:
+        return pk
+    elif pk < mylist[md - 1]:
+        return find_peak(mylist[:med])
+    else:
+        return find_peak(mylist[med + 1:])
