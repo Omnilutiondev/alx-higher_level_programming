950846281be6f48a3ec5a36e2b500f54a29f22e1
Omnilutiondev <ike.john.agu@gmail.com>
2025-01-21 04:09:28 +0100

    Javascript Objects Scopes and Closures - Task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = list.map(function (num, idx) {
-  return num * idx;
+const newList = list.map(function (num, index) {
+  return num * index;
 });
 
 console.log(list);
