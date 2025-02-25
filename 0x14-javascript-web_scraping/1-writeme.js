abd19445027aa05e708bab20bf577bb1f5bf8be7
Omnilutiondev <ike.john.agu@gmail.com>
2025-02-25 06:05:05 +0100

    0x14javascript-web_scraping - Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const frg = require('frg');
-fs.writeFile(process.argv[2], process.argv[3], error => {
+frg.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
 });
